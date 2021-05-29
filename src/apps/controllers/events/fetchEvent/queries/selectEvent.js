8d5ad7f9401138ca53d0dae6c6e2f3165885bd9d
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-05-29 13:26:22 +0300

    getEvent route

---
+++ src/apps/controllers/events/fetchEvent/queries/selectEvent.js
@@ -0,0 +1,20 @@
+const {
+  submitQuery,
+  camelKeys,
+  getFirst
+} = require("../../../../../lib/database/index");
+const selectEvent = ({ slug }) => submitQuery`
+    SELECT 
+      event_id, 
+      event_name,
+      event_description,
+      event_date,
+      host_id, 
+      image, 
+      slug
+
+    FROM events 
+    LEFT JOIN users ON events.host_id = hosts.host_id
+    WHERE slug = ${slug}
+`;
+module.exports = getFirst(camelKeys(selectEvent));
---
+++ src/apps/controllers/events/getEvent/index.js
@@ -0,0 +1,15 @@
+const handleApiError = require("../../../../../utils/handleApiError");
+const fetchEvent = require("../fetchEvent/index");
+
+const getEvent = async (req, res) => {
+  const { slug } = req.params;
+  try {
+    const { event } = await fetchEvent({ slug });
+
+    res.send({ event });
+  } catch (err) {
+    handleApiError(res, err);
+  }
+};
+
+module.exports = getEvent;
