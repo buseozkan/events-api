b679b3ccd75608131714a1b51352a7e451ae81ca
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-05-29 21:52:35 +0300

    getArtistName and getEventCity added

---
+++ src/apps/controllers/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const getEvents = require("../events/getEvent/index");
+const getArtistName = require("../events/getArtistName/getArtistName");
+const getEventCity = require("../events/getEventCity/getEventCity");
+
+const router = express.Router();
+
+router.get("/getEvents", getEvents);
+router.get("/getArtistName", getArtistName);
+router.get("/getEventCity", getEventCity);
+
+module.exports = router;
