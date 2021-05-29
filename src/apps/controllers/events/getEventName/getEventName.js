ac6e397c3c24d5ea86081fac0c16d6e21a5b4005
buseozkan <buse1.ozkan@gmail.com>
2021-05-29 22:42:15 +0300

    geteventname

--- src/apps/controllers/events/getEventName/getEventName.js
+++ src/apps/controllers/events/getEventName/getEventName.js
@@ -1,14 +1,13 @@
 const handleApiError = require("../../../../../utils/handleApiError");
 const fetchEvent = require("../fetchEvent/index");
 
-const getEventCity = async (req, res) => {
+const getEventName = async (req, res) => {
   const { slug } = req.params;
   try {
-    const { city } = await fetchEvent({ slug });
+    const { name } = await fetchEvent({ slug });
     res.send({ city });
   } catch (err) {
     handleApiError(res, err);
   }
 };
-
-module.exports = getEventCity;
+module.exports = getEventName;
